d6db9352c134c981e35a40fdfb2159383ade0e8b
Leonardo Sena <shadow_kras@hotmail.com>
2019-08-15 14:26:41 -0400

    Criado migration com as tabelas da biblioteca virtual.

--- BibliotecaVirtual.Data/Migrations/20190815182543_Biblioteca.Designer.cs
+++ BibliotecaVirtual.Data/Migrations/20190815182543_Biblioteca.Designer.cs
@@ -4,14 +4,16 @@ using BibliotecaVirtual.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BibliotecaVirtual.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190815182543_Biblioteca")]
+    partial class Biblioteca
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
